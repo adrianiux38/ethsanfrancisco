e0cfea482b193f10bb34fe0266ea9c656dc27249
lvaughan9 <92354625+lvaughan9@users.noreply.github.com>
2022-11-05 16:30:51 -0700

    Cleanup and code improvements

--- loaned/src/App.jsx
+++ loaned/src/App.jsx
@@ -1,253 +1,58 @@
 import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Walletbutton from "./components/walletConnect";
 
 import * as React from "react";
-
 export default function App() {
   return (
     <>
       <div className="columns">
-        <div className="builder-columns div">
-          <div className="builder-column column">
-            <div className="div-2">
-              <div className="div-3">
-                <div className="div-4">
-                  <div className="div-5">
-                    <header className="header">
-                      <picture>
-                        <source
-                          srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1"
-                          type="image/webp"
-                        />
-                        <img
-                          loading="lazy"
-                          src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1"
-                          srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1"
-                          className="image"
-                          alt="logo"
-                        />
-                      </picture>
-                      <div className="builder-image-sizer image-sizer" />
-                    </header>
-                    <div className="div-6">Loaned</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="div-7">
-              <div className="div-8">
-                <picture>
-                  <source
-                    srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca"
-                    type="image/webp"
-                  />
-                  <img
-                    loading="lazy"
-                    src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca"
-                    srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca"
-                    className="image"
-                    alt="logo"
-                  />
-                </picture>
-                <div className="builder-image-sizer image-sizer-2" />
-              </div>
-              <div className="uncollateralized-loan-in-just">
-                <p>Uncollateralized loan in just a minute</p>
-              </div>
-            </div>
-            <div className="log-in-lens-metamask-walle">
-              Log in LENS / Metamask / Wallet connect
-            </div>
+        <div className="landing">
+          <header className="header">
+            <picture>
+              <source
+                srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1"
+                type="image/webp"
+              />
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1"
+                srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F636434d8296044d388768d4b8581a5b1"
+                className="image"
+                alt="logo"
+              />
+            </picture>
+            <div className="builder-image-sizer image-sizer" />
+          </header>
+          <div className="logo">Loaned</div>
+        </div>
+        <div className="body">
+          <div className="runner">
+            <picture>
+              <source
+                srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?format=webp&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca"
+                type="image/webp"
+              />
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca"
+                srcSet="https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2FTEMP%2F64bf4e740cdd4ee999fe28b1739d59ca"
+                className="image"
+                alt="logo"
+              />
+            </picture>
+            <div className="builder-image-sizer image-sizer-2" />
+          </div>
+          <div className="uncollateralized-loan-in-just">
+            <p>Uncollateralized loan in just a minute</p>
+          </div>
+          <div className="button-container">
+            <button>Log in LENS / Metamask / Wallet connect</button>
           </div>
         </div>
+        <div className="footer"></div>
       </div>
-      <style jsx>{`
-        background-color: rgba(223, 255, 28, 1);
-        .columns {
-          display: flex;
-          flex-direction: column;
-          max-width: 432px;
-          background-color: rgba(223, 255, 28, 1);
-        }
-        .div {
-          display: flex;
-          background-color: rgba(223, 255, 28, 1);
-        }
-        @media (max-width: 999px) {
-          .div {
-            flex-direction: column;
-            align-items: stretch;
-          }
-        }
-        .column {
-          display: flex;
-          flex-direction: column;
-          line-height: normal;
-          width: calc(60.93% - 0px);
-          margin-left: 0px;
-        }
-        @media (max-width: 999px) {
-          .column {
-            width: 100%;
-          }
-        }
-        .div-2 {
-          display: flex;
-          flex-direction: column;
-          max-width: 263.8399658203125px;
-          justify-content: center;
-          align-items: center;
-          margin-top: 50px;
-        }
-        @media (max-width: 640px) {
-          .div-2 {
-            margin-left: auto;
-            margin-right: auto;
-          }
-        }
-        .div-3 {
-          display: flex;
-          flex-direction: row;
-        }
-        .div-4 {
-          display: flex;
-          flex-direction: column;
-        }
-        .div-5 {
-          display: flex;
-          flex-direction: row;
-        }
-        .header {
-          display: flex;
-          position: relative;
-          min-width: 20px;
-          min-height: 20px;
-          max-width: 14px;
-          width: 14px;
-        }
-        @media (max-width: 640px) {
-          .header {
-            margin-right: auto;
-            background-color: rgba(223, 255, 28, 1);
-          }
-        }
-        .image {
-          object-fit: contain;
-          object-position: center;
-          position: absolute;
-          height: 100%;
-          width: 100%;
-          top: 0;
-          left: 0;
-        }
-        .image-sizer {
-          width: 100%;
-          padding-top: 142.85714285714286%;
-          pointer-events: none;
-          font-size: 0;
-        }
-        .div-6 {
-          max-width: 71px;
-          margin-left: 8.66340160369873px;
-          color: rgba(11, 10, 10, 1);
-          font-size: 18.901966094970703px;
-          line-height: 120.00000476837158%;
-          letter-spacing: 0%;
-          text-align: center;
-          font-family: "Poppins", sans-serif;
-          background-color: rgba(223, 255, 28, 1);
-        }
-        @media (max-width: 640px) {
-          .div-6 {
-            margin-left: auto;
-            margin-top: auto;
-            margin-bottom: auto;
-            margin-right: auto;
-            padding-left: 10px;
-          }
-        }
-        .div-7 {
-          display: flex;
-          flex-direction: column;
-          position: relative;
-          margin-top: 20px;
-          height: 200px;
-        }
-        @media (max-width: 640px) {
-          .div-7 {
-            height: auto;
-            padding-bottom: 30px;
-          }
-        }
-        .div-8 {
-          display: flex;
-          position: relative;
-          min-width: 20px;
-          min-height: 20px;
-          max-width: 186px;
-          width: 186px;
-          margin-top: 50px;
-        }
-        @media (max-width: 640px) {
-          .div-8 {
-            margin-left: auto;
-            margin-right: auto;
-            padding-bottom: 50px;
-          }
-        }
-        .image-sizer-2 {
-          width: 100%;
-          padding-top: 82.79569892473118%;
-          pointer-events: none;
-          font-size: 0;
-        }
-        .uncollateralized-loan-in-just {
-          max-width: 250px;
-          color: rgba(11, 10, 10, 1);
-          font-size: 25.202619552612305px;
-          line-height: 120.00000476837158%;
-          letter-spacing: 0%;
-          text-align: center;
-          font-family: "IBM Plex Sans", sans-serif;
-        }
-        @media (max-width: 640px) {
-          .uncollateralized-loan-in-just {
-            margin-left: auto;
-            margin-right: auto;
-          }
-        }
-        .log-in-lens-metamask-walle {
-          display: flex;
-          flex-direction: column;
-          position: relative;
-          margin-top: 20px;
-          height: 200px;
-          max-width: 234px;
-          color: rgba(0, 0, 0, 1);
-          font-size: 12.601309776306152px;
-          line-height: 150%;
-          letter-spacing: 0%;
-          text-align: left;
-          font-family: "Roboto", sans-serif;
-        }
-        @media (max-width: 640px) {
-          .log-in-lens-metamask-walle {
-            height: auto;
-            padding-bottom: 8px;
-            margin-bottom: 200px;
-            margin-left: auto;
-            margin-right: auto;
-            padding-right: 8px;
-            border-width: 1px;
-            border-style: solid;
-            max-width: 276px;
-            border-radius: 5px;
-            padding-left: 8px;
-            padding-top: 8px;
-          }
-        }
-      `}</style>
     </>
   );
 }
