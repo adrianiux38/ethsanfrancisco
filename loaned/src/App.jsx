efc728a3b8cd1e53b17bc52b8071b41d01d72359
lvaughan9 <92354625+lvaughan9@users.noreply.github.com>
2022-11-06 05:10:15 -0800

    Page 2 and polishes/demo data

--- loaned/src/App.jsx
+++ loaned/src/App.jsx
@@ -1,4 +1,6 @@
 import logo from "./logo.svg";
+import options from "./options.png";
+import loanHistory from "./loanHistory.png";
 import * as React from "react";
 import { useState } from "react";
 import "./App.css";
@@ -62,6 +64,7 @@ function App() {
   const connected = data.isConnected;
   const [submited, changeStatus] = useState("");
   const [value, setValue] = React.useState(0);
+  const [page, setPage] = React.useState(true);
 
   const verifyData = async () => {
     console.log(data);
@@ -98,109 +101,111 @@ function App() {
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <div className="main">
-          <div id="wrapper">
-            <Box
-              id="loader"
-              sx={{
-                justifyContent: "center",
-                alignContent: "center",
-                display: "none",
-              }}
-            ></Box>
-            ;
-            <FadeIn>
-              <div
-                style={{
-                  display: "flex",
-                  width: "100vw",
-                  height: "6vh",
-                  alignContent: "center",
-                  marginLeft: "4vw",
-                  justifyContent: "center",
-                  backgroundColor: "transparent",
-                }}
-              >
-                <img
-                  src={logo}
-                  style={{
-                    width: "200px",
-                    height: "80px",
-                    marginTop: "20%",
-                    display: "flex",
-                    backgroundColor: "transparent",
-                  }}
-                  alt="logo"
-                />
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  marginTop: "20%",
-                  alignContent: "center",
-                  paddingBottom: "20%",
-                  paddingTop: "7vh",
-                }}
-              >
-                {/* <ComponentButton style={{display:'flex', paddingRight:'40px', marginRight:'60px'}}/>*/}
-                <h1
+          {page ? (
+            <div id="wrapper">
+              <FadeIn>
+                <div
                   style={{
                     display: "flex",
                     width: "100vw",
-                    marginTop: "6%",
-                    marginBottom: "10%",
+                    height: "6vh",
+                    alignContent: "center",
+                    marginLeft: "4vw",
                     justifyContent: "center",
+                    backgroundColor: "transparent",
                   }}
                 >
-                  {" "}
-                  Ask for your first loan:
-                </h1>
-                <FadeIn delay={250}>
-                  <WalletButton />
-                </FadeIn>
-                <div className="info">
-                  <p
+                  <img
+                    src={logo}
+                    style={{
+                      width: "200px",
+                      height: "80px",
+                      marginTop: "20%",
+                      display: "flex",
+                      backgroundColor: "transparent",
+                    }}
+                    alt="logo"
+                  />
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: "20%",
+                    alignContent: "center",
+                    paddingBottom: "20%",
+                    paddingTop: "7vh",
+                  }}
+                >
+                  {/* <ComponentButton style={{display:'flex', paddingRight:'40px', marginRight:'60px'}}/>*/}
+                  <h1
                     style={{
                       display: "flex",
                       width: "100vw",
-                      marginTop: "10%",
-                      alignContent: "center",
+                      marginTop: "6%",
+                      marginBottom: "10%",
                       justifyContent: "center",
                     }}
                   >
                     {" "}
-                    Login using Lens:
-                  </p>
+                    Ask for your first loan:
+                  </h1>
+                  <FadeIn delay={250}>
+                    <WalletButton />
+                  </FadeIn>
+                  <div className="info">
+                    <p
+                      style={{
+                        display: "flex",
+                        width: "100vw",
+                        marginTop: "10%",
+                        alignContent: "center",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {" "}
+                      Login using Lens:
+                    </p>
+                  </div>
+                  <div className="center-container">
+                    <form onSubmit={handleSubmit}>
+                      <TextField
+                        id="handleInput"
+                        label="lens handle"
+                        variant="standard"
+                      />
+                      <div className="button-container">
+                        <SuccessModal />
+                      </div>
+                    </form>
+                    <p style={{ fontSize: "10px" }}>{submited}</p>
+                  </div>
                 </div>
-                <div className="center-container">
-                  <form onSubmit={handleSubmit}>
-                    <TextField
-                      id="handleInput"
-                      label="lens handle"
-                      variant="standard"
-                    />
-                    <div className="button-container">
-                      <SuccessModal />
-                    </div>
-                  </form>
-                  <p style={{ fontSize: "10px" }}>{submited}</p>
-                </div>
-              </div>
-              <BottomNavigation
-                showLabels
-                value={value}
-                onChange={(event, newValue) => {
-                  setValue(newValue);
-                }}
-              >
-                <BottomNavigationAction
-                  label="Borrow"
-                  icon={<CallReceivedIcon />}
-                />
-                <BottomNavigationAction label="Repay" icon={<CallMadeIcon />} />
-              </BottomNavigation>
-            </FadeIn>
-          </div>
+                <BottomNavigation
+                  showLabels
+                  value={value}
+                  onChange={(event, newValue) => {
+                    setValue(newValue);
+                  }}
+                >
+                  <BottomNavigationAction
+                    label="Borrow"
+                    icon={<CallReceivedIcon />}
+                  />
+                  <BottomNavigationAction
+                    onClick={() => {
+                      setPage(false);
+                    }}
+                    label="Repay"
+                    icon={<CallMadeIcon />}
+                  />
+                </BottomNavigation>
+              </FadeIn>
+            </div>
+          ) : (
+            //page 2
+            <Page2 />
+          )}
         </div>
       </RainbowKitProvider>
     </WagmiConfig>
@@ -208,3 +213,155 @@ function App() {
 }
 
 export default App;
+
+export function Page2() {
+  return (
+    <div>
+      <FadeIn>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              width: "100vw",
+              height: "6vh",
+              alignContent: "center",
+              marginLeft: "4vw",
+              justifyContent: "center",
+              backgroundColor: "transparent",
+            }}
+          >
+            <img
+              src={logo}
+              style={{
+                width: "170px",
+                height: "80px",
+                marginTop: "5%",
+                display: "flex",
+                backgroundColor: "transparent",
+              }}
+              alt="logo"
+            />
+          </div>
+          <h2 style={{ width: "300px", display: "flex" }}>Payment dashboard</h2>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100vw",
+            marginTop: "12%",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              flex: 0.5,
+              flexDirection: "column",
+            }}
+          >
+            <p
+              style={{
+                fontFamily: "futura",
+                fontSize: "20px",
+                fontWeight: "bold",
+                marginLeft: "8%",
+              }}
+            >
+              Pending Loans
+            </p>
+            <p
+              style={{
+                fontFamily: "futura",
+                marginTop: "-7%",
+                marginLeft: "8%",
+                fontSize: "12px",
+                fontWeight: "lighter",
+              }}
+            >
+              Interest dashboard
+            </p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flex: 0.5,
+              justifyContent: "center",
+              alignContent: "center",
+              fontSize: "20px",
+              marginTop: "6%",
+              fontWeight: "bold",
+            }}
+          >
+            <p>-131$APE</p>
+          </div>
+        </div>
+        <img
+          src={options}
+          style={{
+            width: "100%",
+            height: "6%",
+            marginTop: "5%",
+            marginBottom: "15%",
+            display: "flex",
+            backgroundColor: "transparent",
+          }}
+          alt="options"
+        />
+        <div style={{ width: "100vw", display: "flex", flexDirection: "row" }}>
+          <p
+            style={{
+              display: "flex",
+              fontFamily: "futura",
+              fontSize: "20px",
+              fontWeight: "bold",
+              marginLeft: "4%",
+              flex: 1,
+            }}
+          >
+            Recent Loans
+          </p>
+          <p
+            style={{
+              display: "flex",
+              flex: 1,
+              justifyContent: "center",
+              alignContent: "center",
+              marginTop: "6%",
+            }}
+          >
+            See all
+          </p>
+        </div>
+
+        <FadeIn delay="100" duration="1000">
+          <img
+            src={loanHistory}
+            style={{
+              width: "100%",
+              height: "6%",
+              marginTop: "5%",
+              marginLeft: "5%",
+              marginBottom: "15%",
+              display: "flex",
+              backgroundColor: "transparent",
+            }}
+            alt="loanHistory"
+          />
+        </FadeIn>
+      </FadeIn>
+      <BottomNavigation showLabels onChange={(event, newValue) => {}}>
+        <BottomNavigationAction label="Borrow" icon={<CallReceivedIcon />} />
+        <BottomNavigationAction
+          onClick={() => {}}
+          label="Repay"
+          icon={<CallMadeIcon />}
+        />
+      </BottomNavigation>
+    </div>
+  );
+}
